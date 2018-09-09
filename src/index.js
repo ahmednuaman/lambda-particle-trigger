e5c3d85e09ba59e03691be48cdf1a0eae9e5e84b
Ahmed Nuaman <ahmed@ahmednuaman.com>
2018-09-09 19:36:20 +0100

    updated ready to test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { DynamoDB } from 'aws-sdk'
 import async from 'async'
 import request from 'request'
 
@@ -5,13 +6,57 @@ const {
   particle: config
 } = JSON.parse(process.env.CONFIG)
 
-export const handler = ({ body }, context, done) => {
-  const {
-    state
-  } = JSON.parse(body)
+export const handler = async ({ data }, context, done) => {
+  let state
+
+  if (data) {
+    state = data.state
+  } else {
+    const doc = new DynamoDB.DocumentClient({ service: new DynamoDB({...config.aws, endpoint}) })
+    const params = {
+      TableName: 'iot_house',
+      Key: {
+        button: 'garden_lights'
+      }
+    }
+
+    AWSXRay.captureAWSClient(doc.service)
+
+    try {
+      state = await new Promise((resolve, reject) =>
+        doc.get(params, (error, { Item: item }) => {
+          if (error || !item) {
+            return reject(error || 'Item not found')
+          }
+
+          const {
+            state
+          } = item
+
+          const newState = !state
+
+          doc.put({
+            TableName: params.TableName,
+            Item: {
+              ...params.Key,
+              state: newState
+            }
+          }, (error) => {
+            if (error) {
+              return reject(error)
+            }
+
+            resolve(newState)
+          })
+        })
+      )
+    } catch (error) {
+      return done(error)
+    }
+  }
 
   if (config && state) {
-    const update = (done, particle, state) => {
+    const update = (done, particle) => {
       request.post(`https://api.particle.io/v1/devices/${particle}/relay`, {
         form: {
           access_token: config.access_token,
@@ -20,6 +65,6 @@ export const handler = ({ body }, context, done) => {
       }, done)
     }
 
-    async.parallel(config.particles.map((done) => (particle) => update(done, particle, state)), done)
+    async.parallel(config.particles.map((done) => (particle) => update(done, particle)), done)
   }
 }
